ed92729102ad0bd44e8b012735df02e381d97c78
Nikhilj1806 <166896022+Nikhilj1806@users.noreply.github.com>
2024-04-13 11:22:24 +0530

    Update ccpractical.py

--- ccpractical.py
+++ ccpractical.py
@@ -1,2 +1,10 @@
-print("hello world")
-print ("nikhil")
+
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+
+sum = float(num1) + float(num2)
+
+
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
